e86acadbda6f646808c4c166adbf5811b2cd82f7
petri.ryynanen <petri.ryynanen@CPX-O73OAILTNM9.dir.svc.accenture.com>
2018-12-25 21:53:39 +0200

    Initial commit

---
+++ Cucumber_With_Java-Build_Automation_Framework_In_Lesser_Code/src/main/java/Cucumber_Training/Cucumber_With_Java_Build_Automation_Framework_In_Lesser_Code/App.java
@@ -0,0 +1,13 @@
+package Cucumber_Training.Cucumber_With_Java_Build_Automation_Framework_In_Lesser_Code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
